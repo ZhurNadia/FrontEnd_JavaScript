7e7637f930f3d54c0c3408c9b1a9964134b83e3d
nadia <nadiazh10@gmail.com>
2015-08-06 00:29:57 +0300

    lesson1 task1

---
+++ lesson_1/lesson1_js1.js
@@ -0,0 +1,8 @@
+/**
+ * Объявите две переменные: developer и name. Запишите в name строку 'Sergey'.
+ * Скопируйте значение из name в developer. Выведите developer (должно вывести 'Sergey').
+ */
+var developer, name;
+name = 'Sergey';
+developer = 'Sergey';
+alert(developer);
