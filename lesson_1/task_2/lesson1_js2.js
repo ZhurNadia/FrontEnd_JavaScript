78227f0c165595955bb8b47fcce620f16c429068
nadia <nadiazh10@gmail.com>
2015-08-06 19:18:15 +0300

    lesson1 task2

--- lesson_1/task_2/lesson1_js2.js
+++ lesson_1/task_2/lesson1_js2.js
@@ -2,5 +2,8 @@
  * Создайте страницу, которая спрашивает Ваше имя и выводит его в диалоговом окне.
  */
 
-var name = prompt('What is your name', 'Nadia');
+//var name = prompt('What is your name', 'Nadia');
+//alert(name);
+var name = prompt('What is your name');
 alert(name);
+console.log(name);
