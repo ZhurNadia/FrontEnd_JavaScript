381427bfa94f9f6c93eb574385ecd3f2496d043d
nadia <nadiazh10@gmail.com>
2015-08-18 01:00:27 +0300

    for me

---
+++ reading/SeaWar/seaWar.js
@@ -0,0 +1,10 @@
+/**
+ * Created by nadia on 12.08.15.
+ */
+var location1, location2, location3;
+var guess;
+var hits = 0;
+var guesses = 0;
+var isSunk = false;
+var goal = prompt('Ready? Fire!', 'Enter number position of sunk');
+//p 81-88
